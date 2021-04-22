390cb0ce7a9aa5f986f8f499b20771b8455e5dad
SahatasLee <Sahataslee@outlook.co.th>
2021-04-22 11:33:25 +0700

    submit Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import NavBar from 'react-bootstrap/NavBar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import CreateStudent from './components/create-student.component'
+import EditStudent from './components/edit-student.component'
+import StudentList from './components/student-list.component'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar bg="dark" variant="dark">
+          <Container>
+            <NavBar.Brand>
+              <Link to={"/create-student"} className="nav-Link">
+                React MERN
+                </Link>
+            </NavBar.Brand>
+
+            <Nav className="justify-content-end">
+              <Nav>
+                <Link to={"/create-student"} className="nav-link">
+                  Create Student
+                </Link>
+              </Nav>
+              <Nav>
+                <Link to={"/student-list"} className="nav-link">
+                  Student List
+                </Link>
+              </Nav>
+            </Nav>
+
+          </Container>
+        </NavBar>
+
+        <Container>
+          <Row>
+            <Col cm="12">
+              <div className="wrapper">
+                <Switch>
+                  <Route exact path="/" component={CreateStudent} />
+                  <Route exact path="/create-student" component={CreateStudent} />
+                  <Route exact path="/edit-student/:id" component={EditStudent} />
+                  <Route exact path="/student-list" component={StudentList} />
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
